a9c2f901bde491ab0c34662ad83ef84a082480ae
kraglalbert <albert.kragl@mail.mcgill.ca>
2019-10-22 13:48:14 -0400

    Add configuration for DataSource with a dotenv file

---
+++ Cooperator-Backend/src/main/java/ca/mcgill/cooperator/config/DataSourceConfig.java
@@ -0,0 +1,23 @@
+package ca.mcgill.cooperator.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+     
+    @Bean
+    public DataSource getDataSource() {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url("jdbc:" + dotenv.get("SPRING_DATASOURCE_URL"));
+        dataSourceBuilder.username("SPRING_DATASOURCE_USERNAME");
+        dataSourceBuilder.password("SPRING_DATASOURCE_PASSWORD");
+        return dataSourceBuilder.build();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Cooperator - DP27
+
+## Backend
+
+To run the backend, run `cd Cooperator-Backend` from the root directory and then run `mvn spring-boot:run`. To run tests, run `mvn test`.
