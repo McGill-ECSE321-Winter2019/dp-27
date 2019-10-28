6f42f64e27f77db173141560168bd1650a48a6f5
kraglalbert <albert.kragl@mail.mcgill.ca>
2019-10-28 19:06:20 -0400

    Add initial dummy tests for service methods (#12)

---
+++ Cooperator-Backend/src/test/java/ca/mcgill/cooperator/service/CooperatorServiceAdminTests.java
@@ -0,0 +1,56 @@
+package ca.mcgill.cooperator.service;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CooperatorServiceAdminTests {
+	
+	// TODO: add Service and Repository class imports here
+
+	@BeforeEach
+	@AfterEach
+	public void clearDatabase() {
+		// TODO: clear every repository here
+	}
+	
+	@Test
+	public void testCreateAdmin() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testCreateAdminNull() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testCreateAdminEmpty() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testCreateAdminSpaces() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testUpdateAdmin() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testUpdateAdminInvalid() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testDeleteAdmin() {
+		assertTrue(true);
+	}
+	
+}
---
+++ Cooperator-Backend/src/test/java/ca/mcgill/cooperator/service/CooperatorServiceCoopTests.java
@@ -0,0 +1,56 @@
+package ca.mcgill.cooperator.service;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CooperatorServiceCoopTests {
+	
+	// TODO: add Service and Repository class imports here
+
+	@BeforeEach
+	@AfterEach
+	public void clearDatabase() {
+		// TODO: clear every repository here
+	}
+	
+	@Test
+	public void testCreateCoop() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testCreateCoopNull() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testCreateCoopEmpty() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testCreateCoopSpaces() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testUpdateCoop() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testUpdateCoopInvalid() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testDeleteCoop() {
+		assertTrue(true);
+	}
+
+}
