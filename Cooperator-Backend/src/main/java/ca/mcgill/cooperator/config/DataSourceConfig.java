6ed7b2e72bf89f37ec279273d05f6a118d98c212
Albert Kragl <albert.kragl@mail.mcgill.ca>
2019-10-31 13:08:17 -0400

    Add Java formatter plugin to Maven
    * Add Java formatter plugin to Maven
    
    * Add instructions in README for how to run the formatter

--- Cooperator-Backend/src/main/java/ca/mcgill/cooperator/config/DataSourceConfig.java
+++ Cooperator-Backend/src/main/java/ca/mcgill/cooperator/config/DataSourceConfig.java
@@ -1,16 +1,14 @@
 package ca.mcgill.cooperator.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
-
 import javax.sql.DataSource;
-
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DataSourceConfig {
-     
+
     @Bean
     public DataSource getDataSource() {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Backend
 
 To run the backend, run `cd Cooperator-Backend` from the root directory and then run `mvn spring-boot:run`. To run tests, run `mvn test`.
+
+To format all Java files, make sure you're in `Cooperator-Backend` then run `mvn com.coveo:fmt-maven-plugin:format`.
