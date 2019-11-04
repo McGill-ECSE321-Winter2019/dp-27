bcc20858820a5d97baa873e209e0f7ba65f6c628
PaulHooley <paul.hooley@mail.mcgill.ca>
2019-11-04 14:56:39 -0500

    Rename reportItemRepo to reportSectionRepo

--- Cooperator-Backend/src/main/java/ca/mcgill/cooperator/dao/ReportItemRepository.java
+++
@@ -1,6 +0,0 @@
-package ca.mcgill.cooperator.dao;
-
-import ca.mcgill.cooperator.model.ReportItem;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ReportItemRepository extends CrudRepository<ReportItem, Integer> {}
