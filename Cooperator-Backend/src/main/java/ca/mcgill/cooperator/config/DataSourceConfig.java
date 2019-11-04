1c6c5ef664a5eedf95e5fac09aa4d884fe27c20a
kraglalbert <albert.kragl@mail.mcgill.ca>
2019-11-04 11:03:47 -0500

    Change datasource config to return HikariDataSource

--- Cooperator-Backend/src/main/java/ca/mcgill/cooperator/config/DataSourceConfig.java
+++ Cooperator-Backend/src/main/java/ca/mcgill/cooperator/config/DataSourceConfig.java
@@ -1,20 +1,23 @@
 package ca.mcgill.cooperator.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import javax.sql.DataSource;
+
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 public class DataSourceConfig {
 
 	@Primary
     @Bean
-    public DataSource getDataSource() {
+    public HikariDataSource getDataSource() {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        DataSourceBuilder<HikariDataSource> dataSourceBuilder = DataSourceBuilder.create().type(HikariDataSource.class);
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
         dataSourceBuilder.url(dotenv.get("SPRING_DATASOURCE_URL"));
         dataSourceBuilder.username(dotenv.get("SPRING_DATASOURCE_USERNAME"));
         dataSourceBuilder.password(dotenv.get("SPRING_DATASOURCE_PASSWORD"));
