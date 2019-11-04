108a74b152c1f7b372544171d1111a0516995ae9
kraglalbert <albert.kragl@mail.mcgill.ca>
2019-11-04 10:25:50 -0500

    Revert previos commit and add @Primary annotation to datasource

--- Cooperator-Backend/src/main/java/ca/mcgill/cooperator/config/DataSourceConfig.java
+++ Cooperator-Backend/src/main/java/ca/mcgill/cooperator/config/DataSourceConfig.java
@@ -5,20 +5,19 @@ import javax.sql.DataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DataSourceConfig {
 
+	@Primary
     @Bean
     public DataSource getDataSource() {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(dotenv.get("SPRING_DATASOURCE_URL"));
-        dataSource.setUsername(dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        dataSource.setPassword(dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-
-        return dataSource;
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(dotenv.get("SPRING_DATASOURCE_URL"));
+        dataSourceBuilder.username(dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        dataSourceBuilder.password(dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        return dataSourceBuilder.build();
     }
 }
