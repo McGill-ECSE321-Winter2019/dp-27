c37eaa5d319e13d3f520f895e7508b9658ab94a4
kraglalbert <albert.kragl@mail.mcgill.ca>
2019-11-04 10:20:58 -0500

    Change DataSource config to create datasource manually

--- Cooperator-Backend/src/main/java/ca/mcgill/cooperator/config/DataSourceConfig.java
+++ Cooperator-Backend/src/main/java/ca/mcgill/cooperator/config/DataSourceConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class DataSourceConfig {
@@ -12,10 +13,12 @@ public class DataSourceConfig {
     @Bean
     public DataSource getDataSource() {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url(dotenv.get("SPRING_DATASOURCE_URL"));
-        dataSourceBuilder.username(dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        dataSourceBuilder.password(dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-        return dataSourceBuilder.build();
+        
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(dotenv.get("SPRING_DATASOURCE_URL"));
+        dataSource.setUsername(dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        dataSource.setPassword(dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
+        return dataSource;
     }
 }
