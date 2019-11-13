c9a5dd1d62c36945b4b4a039fc64e4eb48ef495c
kraglalbert <albert.kragl@mail.mcgill.ca>
2019-11-13 18:03:04 -0500

    Add Admin and Company DTOs

---
+++ Cooperator-Backend/src/main/java/ca/mcgill/cooperator/dto/CompanyDto.java
@@ -0,0 +1,38 @@
+package ca.mcgill.cooperator.dto;
+
+import ca.mcgill.cooperator.model.EmployerContact;
+import java.util.List;
+
+public class CompanyDto {
+
+    private int id;
+    private String name;
+
+    private List<EmployerContact> employees;
+
+    public CompanyDto(int id, String name, List<EmployerContact> employees) {
+        this.id = id;
+        this.name = name;
+        this.employees = employees;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<EmployerContact> getEmployees() {
+        return this.employees;
+    }
+
+    public void setEmployees(List<EmployerContact> employees) {
+        this.employees = employees;
+    }
+}
