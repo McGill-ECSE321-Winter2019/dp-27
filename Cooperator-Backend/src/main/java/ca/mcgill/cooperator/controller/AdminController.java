20fb81c01f712ccd7c841cbd87601f5f42baa48a
kraglalbert <albert.kragl@mail.mcgill.ca>
2019-11-13 18:10:47 -0500

    Add convertToDto methods for Admin and Company

--- Cooperator-Backend/src/main/java/ca/mcgill/cooperator/controller/AdminController.java
+++ Cooperator-Backend/src/main/java/ca/mcgill/cooperator/controller/AdminController.java
@@ -1,5 +1,9 @@
 package ca.mcgill.cooperator.controller;
 
+import ca.mcgill.cooperator.dto.AdminDto;
+import ca.mcgill.cooperator.model.Admin;
+import ca.mcgill.cooperator.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("admins")
 public class AdminController {
 
+    @Autowired private AdminService adminService;
+
     @GetMapping("/{id}")
     public String getAdminById(@PathVariable int id) {
         return "Hello World";
+    }
+
+    private AdminDto convertToDto(Admin a) {
+        if (a == null) {
+            throw new IllegalArgumentException("Admin does not exist!");
+        }
+        return new AdminDto(
+                a.getId(),
+                a.getFirstName(),
+                a.getLastName(),
+                a.getEmail(),
+                a.getSentNotifications());
     }
 }
--- Cooperator-Backend/src/main/java/ca/mcgill/cooperator/controller/CompanyController.java
+++ Cooperator-Backend/src/main/java/ca/mcgill/cooperator/controller/CompanyController.java
@@ -1,5 +1,9 @@
 package ca.mcgill.cooperator.controller;
 
+import ca.mcgill.cooperator.dto.CompanyDto;
+import ca.mcgill.cooperator.model.Company;
+import ca.mcgill.cooperator.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("companies")
 public class CompanyController {
 
+    @Autowired private CompanyService companyService;
+
     @GetMapping("/{id}")
     public String getCompanyById(@PathVariable int id) {
         return "Hello World";
+    }
+
+    private CompanyDto convertToDto(Company c) {
+        if (c == null) {
+            throw new IllegalArgumentException("Company does not exist!");
+        }
+        return new CompanyDto(c.getId(), c.getName(), c.getEmployees());
     }
 }
