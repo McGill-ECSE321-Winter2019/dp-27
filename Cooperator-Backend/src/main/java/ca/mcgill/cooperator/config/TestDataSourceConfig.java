51b61c9cf87037bee1894a17459d27a6f944edaf
Albert Kragl <albert.kragl@mail.mcgill.ca>
2019-11-13 13:46:28 -0500

    Set up test database configuration (#27)
    * Create new Configuration bean for test datasource (#19)
    
    * Create properties file for testing and update Configuration bean to read from it

---
+++ Cooperator-Backend/src/main/java/ca/mcgill/cooperator/config/TestDataSourceConfig.java
@@ -0,0 +1,27 @@
+package ca.mcgill.cooperator.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import javax.sql.DataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application-test.properties")
+public class TestDataSourceConfig {
+
+    @Bean
+    @Profile("test")
+    public DataSource getTestDataSource() {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(dotenv.get("TEST_DATASOURCE_URL"));
+        dataSourceBuilder.username(dotenv.get("TEST_DATASOURCE_USERNAME"));
+        dataSourceBuilder.password(dotenv.get("TEST_DATASOURCE_PASSWORD"));
+
+        return dataSourceBuilder.build();
+    }
+}
--- Cooperator-Backend/src/test/java/ca/mcgill/cooperator/service/CooperatorServiceCoopDetailsTests.java
+++ Cooperator-Backend/src/test/java/ca/mcgill/cooperator/service/CooperatorServiceCoopDetailsTests.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class CooperatorServiceCoopDetailsTests {
 
     // TODO: add Service and Repository class imports here
