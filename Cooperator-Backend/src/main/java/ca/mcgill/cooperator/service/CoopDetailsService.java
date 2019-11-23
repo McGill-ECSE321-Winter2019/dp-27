3af51d33b42c90a91797a90ed69a2e73528e7b4f
Susan Matuszewski <susan.matuszewski@mail.mcgill.ca>
2019-11-23 00:24:35 -0500

    #12, #37 added service class and tests for CoopDetails

--- Cooperator-Backend/src/main/java/ca/mcgill/cooperator/service/CoopDetailsService.java
+++ Cooperator-Backend/src/main/java/ca/mcgill/cooperator/service/CoopDetailsService.java
@@ -22,14 +22,17 @@ public class CoopDetailsService {
 	@Transactional
 	public CoopDetails createCoopDetails(int payPerHour, int hoursPerWeek, EmployerContact ec, Coop c) {
 		StringBuilder error = new StringBuilder();
-        if (payPerHour == 0) {
-            error.append("Pay Per Hour cannot be empty! ");
+        if (payPerHour < 0) {
+            error.append("Pay Per Hour is invalid! ");
         }
-        if (hoursPerWeek == 0) {
-            error.append("Hours Per Week cannot be empty! ");
+        if (hoursPerWeek <= 0) {
+            error.append("Hours Per Week is invalid! ");
         }
         if (ec == null) {
-        	error.append("Employer Contact cannot be null!");
+        	error.append("Employer Contact cannot be null! ");
+        }
+        if (c == null) {
+        	error.append("Coop cannot be null!");
         }
         if (error.length() > 0) {
             throw new IllegalArgumentException(error.toString().trim());
@@ -57,16 +60,85 @@ public class CoopDetailsService {
 	}
 	
 	@Transactional
+	public CoopDetails getCoopDetails(int id) {
+		CoopDetails cd = coopDetailsRepository.findById(id).orElse(null);
+        if (cd == null) {
+            throw new IllegalArgumentException("Coop Details with ID " + id + " does not exist!");
+        }
+
+        return cd;
+	}
+	
+	@Transactional
 	public List<CoopDetails> getAllCoopDetails() {
 		return ServiceUtils.toList(coopDetailsRepository.findAll());
 	}
 	
 	@Transactional
 	public CoopDetails deleteCoopDetails(CoopDetails cd) {
+		if (cd == null) {
+			throw new IllegalArgumentException("Coop Details to delete cannot be null!");
+		}
 		Coop c = cd.getCoop();
 		c.setCoopDetails(null);
 		coopRepository.save(c);
+		
+		EmployerContact ec = cd.getEmployerContact();
+		List<CoopDetails> details = ec.getCoopDetails();
+		details.remove(cd);
+		ec.setCoopDetails(details);
+		employerContactRepository.save(ec);
+		
 		coopDetailsRepository.delete(cd);
 		return cd;
 	}
+	
+	@Transactional
+	public CoopDetails updateCoopDetails(CoopDetails cd, int payPerHour, int hoursPerWeek, EmployerContact ec, Coop c) {
+		StringBuilder error = new StringBuilder();
+        if (payPerHour < 0) {
+            error.append("Pay Per Hour is invalid! ");
+        }
+        if (hoursPerWeek <= 0) {
+            error.append("Hours Per Week is invalid! ");
+        }
+        if (ec == null) {
+        	error.append("Employer Contact cannot be null! ");
+        }
+        if (c == null) {
+        	error.append("Coop cannot be null!");
+        }
+        if (error.length() > 0) {
+            throw new IllegalArgumentException(error.toString().trim());
+        }
+        
+        cd.setPayPerHour(payPerHour);
+        cd.setHoursPerWeek(hoursPerWeek);
+        cd.setEmployerContact(ec);
+        cd.setCoop(c);
+        
+        coopDetailsRepository.save(cd);
+        
+        List <CoopDetails> employerCoopDetails = ec.getCoopDetails();
+        boolean employerContains = false;
+        for (CoopDetails coopDetails : employerCoopDetails) {
+        	if (coopDetails.getId() == cd.getId()) {
+        		int index = employerCoopDetails.indexOf(coopDetails);
+        		employerCoopDetails.set(index, cd);
+        		employerContains = true;
+        	}
+        }
+        if (employerContains == false) {
+        	employerCoopDetails.add(cd);
+        }
+        ec.setCoopDetails(employerCoopDetails);
+        
+        c.setCoopDetails(cd);
+        
+        employerContactRepository.save(ec);
+        coopRepository.save(c);
+        
+        return coopDetailsRepository.save(cd);
+	}
+	
 }
--- Cooperator-Backend/src/test/java/ca/mcgill/cooperator/service/CooperatorServiceCoopDetailsTests.java
+++ Cooperator-Backend/src/test/java/ca/mcgill/cooperator/service/CooperatorServiceCoopDetailsTests.java
@@ -1,62 +1,265 @@
 package ca.mcgill.cooperator.service;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import ca.mcgill.cooperator.dao.CompanyRepository;
+import ca.mcgill.cooperator.dao.CoopDetailsRepository;
+import ca.mcgill.cooperator.dao.CoopRepository;
+import ca.mcgill.cooperator.dao.CourseOfferingRepository;
+import ca.mcgill.cooperator.dao.CourseRepository;
+import ca.mcgill.cooperator.dao.EmployerContactRepository;
+import ca.mcgill.cooperator.dao.StudentRepository;
+import ca.mcgill.cooperator.model.Company;
+import ca.mcgill.cooperator.model.Coop;
+import ca.mcgill.cooperator.model.CoopDetails;
+import ca.mcgill.cooperator.model.CoopStatus;
+import ca.mcgill.cooperator.model.Course;
+import ca.mcgill.cooperator.model.CourseOffering;
+import ca.mcgill.cooperator.model.EmployerContact;
+import ca.mcgill.cooperator.model.Season;
+import ca.mcgill.cooperator.model.Student;
 
 @SpringBootTest
 @ActiveProfiles("test")
 public class CooperatorServiceCoopDetailsTests {
 
-    // TODO: add Service and Repository class imports here
+    @Autowired CoopDetailsRepository coopDetailsRepository;
+    @Autowired CoopRepository coopRepository;
+	@Autowired CourseRepository courseRepository;
+	@Autowired CourseOfferingRepository courseOfferingRepository;
+	@Autowired CompanyRepository companyRepository;
+	@Autowired EmployerContactRepository employerContactRepository;
+	@Autowired StudentRepository studentRepository; 
+    
+    @Autowired CoopDetailsService coopDetailsService;
+    @Autowired CoopService coopService;
+	@Autowired CourseService courseService;
+	@Autowired CourseOfferingService courseOfferingService;
+	@Autowired CompanyService companyService;
+	@Autowired EmployerContactService employerContactService;
+	@Autowired StudentService studentService;
 
     @BeforeEach
     @AfterEach
     public void clearDatabase() {
-        // TODO: clear every repository here
+    	coopDetailsRepository.deleteAll();
+    	List <CoopDetails> coopDetails = coopDetailsService.getAllCoopDetails();
+    	for(CoopDetails cd : coopDetails) {
+    		coopDetailsService.deleteCoopDetails(cd);
+    	}
+        coopRepository.deleteAll();
+        courseOfferingRepository.deleteAll();
+        courseRepository.deleteAll();
+        employerContactRepository.deleteAll();
+        companyRepository.deleteAll();
+        studentRepository.deleteAll();
     }
 
     @Test
     public void testCreateCoopDetails() {
-        assertTrue(true);
+        int payPerHour = 20;
+        int hoursPerWeek = 40;
+        Company company = createTestCompany();
+        EmployerContact ec = createTestEmployerContact(company);
+        Course course = createTestCourse();
+        CourseOffering courseOffering = createTestCourseOffering(course);
+        Student s = createTestStudent();
+        Coop coop = createTestCoop(courseOffering, s);
+        
+        try {
+        	coopDetailsService.createCoopDetails(payPerHour, hoursPerWeek, ec, coop);
+        } catch (IllegalArgumentException e) {
+        	fail();
+        }
+        
+        assertEquals(1, coopDetailsService.getAllCoopDetails().size());
     }
 
     @Test
     public void testCreateCoopDetailsNull() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void testCreateCoopDetailsEmpty() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void testCreateCoopDetailsSpaces() {
-        assertTrue(true);
+    	int payPerHour = 20;
+        int hoursPerWeek = 40;
+        String error = "";
+        try {
+        	coopDetailsService.createCoopDetails(payPerHour, hoursPerWeek, null, null);
+        } catch (IllegalArgumentException e) {
+        	error = e.getMessage();
+        }
+        
+        assertEquals("Employer Contact cannot be null! "
+        		   + "Coop cannot be null!", error);
+        assertEquals(0, coopDetailsService.getAllCoopDetails().size());
     }
 
     @Test
     public void testCreateCoopDetailsInvalid() {
-        assertTrue(true);
+    	int payPerHour = -1;
+        int hoursPerWeek = -20;
+        String error = "";
+        try {
+        	coopDetailsService.createCoopDetails(payPerHour, hoursPerWeek, null, null);
+        } catch (IllegalArgumentException e) {
+        	error = e.getMessage();
+        }
+        
+        assertEquals("Pay Per Hour is invalid! "
+        		   + "Hours Per Week is invalid! "
+        		   + "Employer Contact cannot be null! "
+        		   + "Coop cannot be null!", error);
+        assertEquals(0, coopDetailsService.getAllCoopDetails().size());
     }
 
     @Test
     public void testUpdateCoopDetails() {
-        assertTrue(true);
+    	CoopDetails cd = null;
+    	int payPerHour = 20;
+        int hoursPerWeek = 40;
+        Company company = createTestCompany();
+        EmployerContact ec = createTestEmployerContact(company);
+        Course course = createTestCourse();
+        CourseOffering courseOffering = createTestCourseOffering(course);
+        Student s = createTestStudent();
+        Coop coop = createTestCoop(courseOffering, s);
+        
+        try {
+        	cd = coopDetailsService.createCoopDetails(payPerHour, hoursPerWeek, ec, coop);
+        } catch (IllegalArgumentException e) {
+        	fail();
+        }
+        
+        assertEquals(1, coopDetailsService.getAllCoopDetails().size());
+        
+        try {
+        	cd = coopDetailsService.updateCoopDetails(cd, 22, 30, ec, coop);
+        } catch (IllegalArgumentException e) {
+        	fail();
+        }
+        
+        assertEquals(22, cd.getPayPerHour());
+        assertEquals(1, coopDetailsService.getAllCoopDetails().size());
     }
 
     @Test
     public void testUpdateCoopDetailsInvalid() {
-        assertTrue(true);
+    	CoopDetails cd = null;
+    	int payPerHour = 20;
+        int hoursPerWeek = 40;
+        Company company = createTestCompany();
+        EmployerContact ec = createTestEmployerContact(company);
+        Course course = createTestCourse();
+        CourseOffering courseOffering = createTestCourseOffering(course);
+        Student s = createTestStudent();
+        Coop coop = createTestCoop(courseOffering, s);
+        
+        try {
+        	cd = coopDetailsService.createCoopDetails(payPerHour, hoursPerWeek, ec, coop);
+        } catch (IllegalArgumentException e) {
+        	fail();
+        }
+        
+        assertEquals(1, coopDetailsService.getAllCoopDetails().size());
+        
+        String error = ""; 
+        try {
+        	cd = coopDetailsService.updateCoopDetails(cd, -1, -222, null, null);
+        } catch (IllegalArgumentException e) {
+        	error = e.getMessage();
+        }
+        
+        assertEquals("Pay Per Hour is invalid! "
+     		       + "Hours Per Week is invalid! "
+     		       + "Employer Contact cannot be null! "
+     		       + "Coop cannot be null!", error);
+        assertEquals(1, coopDetailsService.getAllCoopDetails().size());
     }
 
     @Test
     public void testDeleteCoopDetails() {
-        assertTrue(true);
+    	CoopDetails cd = null;
+    	int payPerHour = 20;
+        int hoursPerWeek = 40;
+        Company company = createTestCompany();
+        EmployerContact ec = createTestEmployerContact(company);
+        Course course = createTestCourse();
+        CourseOffering courseOffering = createTestCourseOffering(course);
+        Student s = createTestStudent();
+        Coop coop = createTestCoop(courseOffering, s);
+        
+        try {
+        	cd = coopDetailsService.createCoopDetails(payPerHour, hoursPerWeek, ec, coop);
+        } catch (IllegalArgumentException e) {
+        	fail();
+        }
+        
+        assertEquals(1, coopDetailsService.getAllCoopDetails().size());
+        
+        try {
+        	coopDetailsService.deleteCoopDetails(cd);
+        } catch (IllegalArgumentException e) {
+        	fail();
+        }
+        
+        assertEquals(0, coopDetailsService.getAllCoopDetails().size());
+    }
+    
+    @Test
+    public void testDeleteCoopDetailsInvalid() {
+    	String error = "";
+    	try {
+    		coopDetailsService.deleteCoopDetails(null);
+    	} catch (IllegalArgumentException e) {
+    		error = e.getMessage();
+    	}
+    	
+    	assertEquals("Coop Details to delete cannot be null!", error);
+    }
+    
+    private Course createTestCourse() {
+    	Course c = null;
+    	c = courseService.createCourse("FACC200");
+    	return c;
+    }
+    
+    private CourseOffering createTestCourseOffering(Course c) {
+    	CourseOffering co = null;
+    	co = courseOfferingService.createCourseOffering(2020, Season.WINTER, c);
+    	return co;
+    }
+    
+    private Coop createTestCoop(CourseOffering co, Student s) {
+    	Coop coop = new Coop();
+    	coop = coopService.createCoop(CoopStatus.FUTURE, co, s);
+    	return coop;
+    }
+    
+    private Company createTestCompany() {
+        Company c = new Company();
+        c = companyService.createCompany("Facebook", null);
+        
+        return c;
+        
+    }
+    
+    private EmployerContact createTestEmployerContact(Company c) {
+    	EmployerContact ec = new EmployerContact();
+    	ec = employerContactService.createEmployerContact("Emma", "Eags", "eags@gmail.com", "2143546578", c);
+    	return ec;
+    }
+    
+    private Student createTestStudent() {
+    	Student s = new Student();
+    	s = studentService.createStudent("Susan", "Matuszewski", "susan@gmail.com", "260719281");
+    	
+    	return s;
     }
 }
