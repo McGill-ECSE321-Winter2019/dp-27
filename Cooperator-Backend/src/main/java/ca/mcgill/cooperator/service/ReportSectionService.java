5aa58e6f18b2732affdccbc9098dac77a3324528
Susan Matuszewski <susan.matuszewski@mail.mcgill.ca>
2019-11-22 22:32:13 -0500

    #12, #21 added service class and tests for report section

--- Cooperator-Backend/src/main/java/ca/mcgill/cooperator/service/ReportSectionService.java
+++ Cooperator-Backend/src/main/java/ca/mcgill/cooperator/service/ReportSectionService.java
@@ -1,16 +1,24 @@
 package ca.mcgill.cooperator.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ca.mcgill.cooperator.dao.EmployerReportRepository;
 import ca.mcgill.cooperator.dao.ReportSectionRepository;
+import ca.mcgill.cooperator.dao.StudentReportRepository;
+import ca.mcgill.cooperator.model.EmployerReport;
 import ca.mcgill.cooperator.model.ReportSection;
+import ca.mcgill.cooperator.model.StudentReport;
 
 @Service
 public class ReportSectionService {
 	
 	@Autowired ReportSectionRepository reportSectionRepository;
+	@Autowired StudentReportRepository studentReportRepository;
+	@Autowired EmployerReportRepository employerReportRepository;
 	
 	@Transactional
 	public ReportSection createReportSection(String title, String content) {
@@ -26,9 +34,114 @@ public class ReportSectionService {
         }
         
         ReportSection rs = new ReportSection();
-        rs.setTitle(title);
-        rs.setContent(content);
+        rs.setTitle(title.trim());
+        rs.setContent(content.trim());
         
         return reportSectionRepository.save(rs);
+	}
+	
+	@Transactional
+	public ReportSection getReportSection(int id) {
+		ReportSection rs = reportSectionRepository.findById(id).orElse(null);
+        if (rs == null) {
+            throw new IllegalArgumentException("Report Section with ID " + id + " does not exist!");
+        }
+
+        return rs;
+	}
+	
+	@Transactional
+	public List<ReportSection> getAllReportSections() {
+		return ServiceUtils.toList(reportSectionRepository.findAll());
+	}
+	
+	@Transactional
+	public ReportSection updateReportSection(ReportSection rs, String title, String content, StudentReport sr, EmployerReport er) {
+		StringBuilder error = new StringBuilder();
+		if (rs == null) {
+			error.append("Report Section cannot be null! ");
+		}
+        if (title == null || title.trim().length() == 0) {
+            error.append("Title cannot be null! ");
+        }
+        if (content == null || content.trim().length() == 0) {
+            error.append("Content cannot be null! ");
+        }
+        if (sr != null && er != null) {
+        	error.append("Cannot add to both Student Report and Employer Report!");
+        }
+        if (error.length() > 0) {
+            throw new IllegalArgumentException(error.toString().trim());
+        }
+        
+        rs.setTitle(title.trim());
+        rs.setContent(content.trim());
+        
+        if (sr != null) {
+        	rs.setStudentReport(sr);
+        	reportSectionRepository.save(rs);
+        	boolean contains = false;
+        	List<ReportSection> sections = sr.getReportSections();
+        	for (ReportSection section : sections) {
+        		if (section.getId() == rs.getId()) {
+        			int index = sections.indexOf(section);
+        			sections.set(index, rs);
+        			contains = true;
+        		}
+        	}
+        	if (contains == false) {
+        		sections.add(rs);
+        	}
+        	sr.setReportSections(sections);
+        	studentReportRepository.save(sr);
+        }
+        
+        if (er != null) {
+        	rs.setEmployerReport(er);
+        	reportSectionRepository.save(rs);
+        	boolean contains = false;
+        	List<ReportSection> sections = er.getReportSections();
+        	for (ReportSection section : sections) {
+        		if (section.getId() == rs.getId()) {
+        			int index = sections.indexOf(section);
+        			sections.set(index, rs);
+        			contains = true;
+        		}
+        	}
+        	if (contains == false) {
+        		sections.add(rs);
+        	}
+        	er.setReportSections(sections);
+        	employerReportRepository.save(er);
+        }
+        
+        return reportSectionRepository.save(rs);
+	}
+	
+	@Transactional
+	public ReportSection deleteReportSection(ReportSection rs) {
+		if (rs == null) {
+			throw new IllegalArgumentException("Report Section to delete cannot be null!");
+		}
+		
+		//first delete from all parents
+		StudentReport sr = rs.getStudentReport();
+		if (sr != null) {
+			List<ReportSection> sections = sr.getReportSections();
+			sections.remove(rs);
+			sr.setReportSections(sections);
+			studentReportRepository.save(sr);
+		}
+		
+		EmployerReport er = rs.getEmployerReport();
+		if (er != null) {
+			List<ReportSection> sections = er.getReportSections();
+			sections.remove(rs);
+			er.setReportSections(sections);
+			employerReportRepository.save(er);
+		}
+		
+		reportSectionRepository.delete(rs);
+		return rs;
 	}
 }
---
+++ Cooperator-Backend/src/test/java/ca/mcgill/cooperator/service/CooperatorServiceReportSectionTests.java
@@ -0,0 +1,412 @@
+package ca.mcgill.cooperator.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import ca.mcgill.cooperator.dao.CompanyRepository;
+import ca.mcgill.cooperator.dao.CoopRepository;
+import ca.mcgill.cooperator.dao.CourseOfferingRepository;
+import ca.mcgill.cooperator.dao.CourseRepository;
+import ca.mcgill.cooperator.dao.EmployerContactRepository;
+import ca.mcgill.cooperator.dao.EmployerReportRepository;
+import ca.mcgill.cooperator.dao.ReportSectionRepository;
+import ca.mcgill.cooperator.dao.StudentReportRepository;
+import ca.mcgill.cooperator.dao.StudentRepository;
+import ca.mcgill.cooperator.model.Company;
+import ca.mcgill.cooperator.model.Coop;
+import ca.mcgill.cooperator.model.CoopStatus;
+import ca.mcgill.cooperator.model.Course;
+import ca.mcgill.cooperator.model.CourseOffering;
+import ca.mcgill.cooperator.model.EmployerContact;
+import ca.mcgill.cooperator.model.EmployerReport;
+import ca.mcgill.cooperator.model.ReportSection;
+import ca.mcgill.cooperator.model.ReportStatus;
+import ca.mcgill.cooperator.model.Season;
+import ca.mcgill.cooperator.model.Student;
+import ca.mcgill.cooperator.model.StudentReport;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class CooperatorServiceReportSectionTests {
+	@Autowired ReportSectionRepository reportSectionRepository;
+	@Autowired StudentReportRepository studentReportRepository;
+	@Autowired EmployerReportRepository employerReportRepository;
+	@Autowired CoopRepository coopRepository;
+	@Autowired CourseRepository courseRepository;
+	@Autowired CourseOfferingRepository courseOfferingRepository;
+	@Autowired CompanyRepository companyRepository;
+	@Autowired EmployerContactRepository employerContactRepository;
+	@Autowired StudentRepository studentRepository;
+	
+	@Autowired ReportSectionService reportSectionService;
+	@Autowired StudentReportService studentReportService;
+	@Autowired EmployerReportService employerReportService;
+	@Autowired CoopService coopService;
+	@Autowired CourseService courseService;
+	@Autowired CourseOfferingService courseOfferingService;
+	@Autowired CompanyService companyService;
+	@Autowired EmployerContactService employerContactService;
+	@Autowired StudentService studentService;
+	
+	@BeforeEach
+    @AfterEach
+    public void clearDatabase() {
+    	coopRepository.deleteAll();
+    	courseOfferingRepository.deleteAll();
+    	courseRepository.deleteAll();
+    	employerContactRepository.deleteAll();
+    	companyRepository.deleteAll();
+    	studentRepository.deleteAll();
+    	reportSectionRepository.deleteAll();
+		employerReportRepository.deleteAll();
+		studentReportRepository.deleteAll();
+	}
+	
+	@Test
+	public void createReportSection() {
+		String title = "Hello";
+		String content = "This is a report section";
+		
+		try {
+			reportSectionService.createReportSection(title, content);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(1, reportSectionService.getAllReportSections().size());
+	}
+	
+	@Test
+	public void createReportSectionNull() {
+		String title = null;
+		String content = null;
+		
+		String error = "";
+		try {
+			reportSectionService.createReportSection(title, content);
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
+		
+		assertEquals("Title cannot be null! "
+				   + "Content cannot be null!", error);
+		assertEquals(0, reportSectionService.getAllReportSections().size());
+	}
+	
+	@Test
+	public void createReportSectionSpaces() {
+		String title = "    ";
+		String content = "            ";
+		
+		String error = "";
+		try {
+			reportSectionService.createReportSection(title, content);
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
+		
+		assertEquals("Title cannot be null! "
+				   + "Content cannot be null!", error);
+		assertEquals(0, reportSectionService.getAllReportSections().size());
+	}
+	
+	@Test
+	public void createReportSectionEmpty() {
+		String title = "";
+		String content = "";
+		
+		String error = "";
+		try {
+			reportSectionService.createReportSection(title, content);
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
+		
+		assertEquals("Title cannot be null! "
+				   + "Content cannot be null!", error);
+		assertEquals(0, reportSectionService.getAllReportSections().size());
+	}
+	
+	@Test
+	public void updateReportSectionStudentReport() {
+		ReportSection rs = null;
+		String title = "Hello";
+		String content = "This is a report section";
+		
+		try {
+			rs = reportSectionService.createReportSection(title, content);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(1, reportSectionService.getAllReportSections().size());
+		
+		Course course = createTestCourse();
+		CourseOffering courseOffering = createTestCourseOffering(course);
+		Student s = createTestStudent();
+		Coop coop = createTestCoop(courseOffering, s);
+		StudentReport sr = createTestStudentReport(coop);
+		
+		try {
+			rs = reportSectionService.updateReportSection(rs, title, content, sr, null);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(sr.getId(), rs.getStudentReport().getId());
+		assertEquals(1, reportSectionService.getAllReportSections().size());
+	}
+	
+	@Test
+	public void updateReportSectionEmployerReport() {
+		ReportSection rs = null;
+		String title = "Hello";
+		String content = "This is a report section";
+		
+		try {
+			rs = reportSectionService.createReportSection(title, content);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(1, reportSectionService.getAllReportSections().size());
+		
+		Course course = createTestCourse();
+		CourseOffering courseOffering = createTestCourseOffering(course);
+		Student s = createTestStudent();
+		Coop coop = createTestCoop(courseOffering, s);
+		Company company = createTestCompany();
+		EmployerContact ec = createTestEmployerContact(company);
+		EmployerReport er = createTestEmployerReport(coop, ec);
+		
+		try {
+			rs = reportSectionService.updateReportSection(rs, title, content, null, er);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(er.getId(), rs.getEmployerReport().getId());
+		assertEquals(1, reportSectionService.getAllReportSections().size());
+	}
+	
+	@Test
+	public void updateReportSectionInvalid() {
+		ReportSection rs = null;
+		String title = "Hello";
+		String content = "This is a report section";
+		
+		try {
+			rs = reportSectionService.createReportSection(title, content);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(1, reportSectionService.getAllReportSections().size());
+		
+		StudentReport sr = new StudentReport();
+		EmployerReport er = new EmployerReport();
+		
+		String error = "";
+		try {
+			rs = reportSectionService.updateReportSection(rs, title, content, sr, er);
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
+		
+		assertEquals("Cannot add to both Student Report and Employer Report!", error);
+	}
+	
+	@Test
+	public void updateReportSectionNull() {
+		String title = "Hello";
+		String content = "This is a report section";
+		
+		try {
+			reportSectionService.createReportSection(title, content);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(1, reportSectionService.getAllReportSections().size());
+		
+		String error = "";
+		try {
+			reportSectionService.updateReportSection(null, null, null, null, null);
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
+		
+		assertEquals("Report Section cannot be null! "
+				   + "Title cannot be null! "
+				   + "Content cannot be null!", error);
+	}
+	
+	@Test
+	public void deleteReportSection() {
+		ReportSection rs = null;
+		String title = "Hello";
+		String content = "This is a report section";
+		
+		try {
+			rs = reportSectionService.createReportSection(title, content);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(1, reportSectionService.getAllReportSections().size());
+		
+		try {
+			reportSectionService.deleteReportSection(rs);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(0, reportSectionService.getAllReportSections().size());
+	}
+	
+	@Test
+	public void deleteReportSectionStudentReport() {
+		ReportSection rs = null;
+		String title = "Hello";
+		String content = "This is a report section";
+		
+		try {
+			rs = reportSectionService.createReportSection(title, content);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(1, reportSectionService.getAllReportSections().size());
+		
+		Course course = createTestCourse();
+		CourseOffering courseOffering = createTestCourseOffering(course);
+		Student s = createTestStudent();
+		Coop coop = createTestCoop(courseOffering, s);
+		StudentReport sr = createTestStudentReport(coop);
+		
+		try {
+			rs = reportSectionService.updateReportSection(rs, title, content, sr, null);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(sr.getId(), rs.getStudentReport().getId());
+		assertEquals(1, reportSectionService.getAllReportSections().size());
+		
+		try {
+			reportSectionService.deleteReportSection(rs);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(0, reportSectionService.getAllReportSections().size());
+	}
+	
+	@Test
+	public void deleteReportSectionEmployerReport() {
+		ReportSection rs = null;
+		String title = "Hello";
+		String content = "This is a report section";
+		
+		try {
+			rs = reportSectionService.createReportSection(title, content);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(1, reportSectionService.getAllReportSections().size());
+		
+		Course course = createTestCourse();
+		CourseOffering courseOffering = createTestCourseOffering(course);
+		Student s = createTestStudent();
+		Coop coop = createTestCoop(courseOffering, s);
+		Company company = createTestCompany();
+		EmployerContact ec = createTestEmployerContact(company);
+		EmployerReport er = createTestEmployerReport(coop, ec);
+		
+		try {
+			rs = reportSectionService.updateReportSection(rs, title, content, null, er);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(er.getId(), rs.getEmployerReport().getId());
+		assertEquals(1, reportSectionService.getAllReportSections().size());
+		
+		try {
+			reportSectionService.deleteReportSection(rs);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(0, reportSectionService.getAllReportSections().size());
+	}
+	
+	@Test
+	public void testDeleteReportSectionInvalid() {
+		String error = "";
+		try {
+			reportSectionService.deleteReportSection(null);
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
+		
+		assertEquals("Report Section to delete cannot be null!", error);
+	}
+	
+	private Course createTestCourse() {
+    	Course c = null;
+    	c = courseService.createCourse("FACC200");
+    	return c;
+    }
+    
+    private CourseOffering createTestCourseOffering(Course c) {
+    	CourseOffering co = null;
+    	co = courseOfferingService.createCourseOffering(2020, Season.WINTER, c);
+    	return co;
+    }
+    
+    private Coop createTestCoop(CourseOffering co, Student s) {
+    	Coop coop = new Coop();
+    	coop = coopService.createCoop(CoopStatus.FUTURE, co, s);
+    	return coop;
+    }
+    
+    private Company createTestCompany() {
+        Company c = new Company();
+        c = companyService.createCompany("Facebook", null);
+        return c;
+        
+    }
+    
+    private Student createTestStudent() {
+    	Student s = new Student();
+    	s = studentService.createStudent("Susan", "Matuszewski", "susan@gmail.com", "260719281");
+    	return s;
+    }
+    
+    private EmployerContact createTestEmployerContact(Company c) {
+    	EmployerContact ec = new EmployerContact();
+    	ec = employerContactService.createEmployerContact("Emma", "Eags", "eags@gmail.com", "2143546578", c);
+    	return ec;
+    }
+	
+	private StudentReport createTestStudentReport(Coop c) {
+		StudentReport sr = new StudentReport();
+		sr = studentReportService.createStudentReport(ReportStatus.COMPLETED, c);
+		return sr;
+	}
+	
+	private EmployerReport createTestEmployerReport(Coop c, EmployerContact ec) {
+		EmployerReport sr = new EmployerReport();
+		sr = employerReportService.createEmployerReport(ReportStatus.COMPLETED, c, ec);
+		return sr;
+	}
+}
