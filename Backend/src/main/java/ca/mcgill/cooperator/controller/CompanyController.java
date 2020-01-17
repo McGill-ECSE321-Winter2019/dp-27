b052d786e3c083dcb1587770417a78ab9f5c17f2
Susan Matuszewski <susan.matuszewski@mail.mcgill.ca>
2020-01-17 10:47:21 -0500

    init start to implementing rest controller

--- Backend/src/main/java/ca/mcgill/cooperator/controller/CompanyController.java
+++ Backend/src/main/java/ca/mcgill/cooperator/controller/CompanyController.java
@@ -1,13 +1,22 @@
 package ca.mcgill.cooperator.controller;
 
 import ca.mcgill.cooperator.dto.CompanyDto;
+import ca.mcgill.cooperator.dto.EmployerContactDto;
 import ca.mcgill.cooperator.model.Company;
+import ca.mcgill.cooperator.model.EmployerContact;
 import ca.mcgill.cooperator.service.CompanyService;
+import ca.mcgill.cooperator.service.EmployerContactService;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*")
@@ -16,10 +25,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class CompanyController {
 
     @Autowired private CompanyService companyService;
+    @Autowired private EmployerContactService employerContactService;
 
-    @GetMapping("/{id}")
-    public String getCompanyById(@PathVariable int id) {
-        return "Hello World";
+    @GetMapping("/company/{id}")
+    public CompanyDto getCompanyById(@PathVariable int id) {
+    	Company c = companyService.getCompany(id);
+        return convertToDto(c);
+    }
+    
+    @GetMapping("/hello")
+    public String helloWorld() {
+    	
+        return "hello bish";
+    }
+    
+    @PostMapping("/company/create")
+    public CompanyDto createCompany(@RequestParam String name, @RequestParam(required = false)List<EmployerContactDto> employerContactDtos) {
+    	List<EmployerContact> employerContacts = new ArrayList<EmployerContact>();
+    	if (employerContactDtos != null) {
+	    	for (EmployerContactDto employerContactDto : employerContactDtos) {
+	    		EmployerContact ec = employerContactService.getEmployerContact(employerContactDto.getId());
+	    		employerContacts.add(ec);
+	    	}
+    	}
+    	Company company = companyService.createCompany(name, employerContacts);
+    	
+    	return convertToDto(company);
     }
 
     private CompanyDto convertToDto(Company c) {
--- Backend/src/main/java/ca/mcgill/cooperator/controller/EmployerContactController.java
+++ Backend/src/main/java/ca/mcgill/cooperator/controller/EmployerContactController.java
@@ -1,22 +1,87 @@
 package ca.mcgill.cooperator.controller;
 
+import ca.mcgill.cooperator.dto.CompanyDto;
+import ca.mcgill.cooperator.dto.CoopDetailsDto;
 import ca.mcgill.cooperator.dto.EmployerContactDto;
+import ca.mcgill.cooperator.dto.EmployerReportDto;
+import ca.mcgill.cooperator.model.Company;
+import ca.mcgill.cooperator.model.CoopDetails;
 import ca.mcgill.cooperator.model.EmployerContact;
+import ca.mcgill.cooperator.model.EmployerReport;
+import ca.mcgill.cooperator.service.CompanyService;
+import ca.mcgill.cooperator.service.CoopDetailsService;
+import ca.mcgill.cooperator.service.EmployerContactService;
+import ca.mcgill.cooperator.service.EmployerReportService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("employer-contacts")
 public class EmployerContactController {
+	
+	@Autowired EmployerContactService employerContactService;
+	@Autowired CompanyService companyService;
+	@Autowired EmployerReportService employerReportService;
+	@Autowired CoopDetailsService coopDetailsService;
 
-    @GetMapping("/{id}")
-    public String getEmployerContactById(@PathVariable int id) {
-        return "Hello World";
+    @GetMapping("/employer/{id}")
+    public EmployerContactDto getEmployerContactById(@PathVariable int id) {
+    	EmployerContact ec = employerContactService.getEmployerContact(id);
+        return convertToDto(ec);
     }
+    
+    @PostMapping("/employer/create")
+	public EmployerContactDto createEmployerContact(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, 
+			@RequestParam String phoneNumber, CompanyDto companyDto) {
+    	
+    	Company company = companyService.getCompany(companyDto.getId());
+		EmployerContact ec = employerContactService.createEmployerContact(firstName, lastName, email, phoneNumber, company);
+		return convertToDto(ec);
+	}
+    
+    @PutMapping("/employer/update")
+    public EmployerContactDto updateEmployerContact(@RequestParam int id, @RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, 
+			@RequestParam String phoneNumber, CompanyDto companyDto, List<EmployerReportDto> employerReportDtos, List<CoopDetailsDto> coopDetailsDtos) {
+    	
+    	EmployerContact ec = employerContactService.getEmployerContact(id);
+    	Company company = companyService.getCompany(companyDto.getId());
+    	
+    	List<EmployerReport> employerReports = new ArrayList<EmployerReport>();
+    	for (EmployerReportDto employerReportDto : employerReportDtos) {
+    		EmployerReport er = employerReportService.getEmployerReport(employerReportDto.getId());
+    		employerReports.add(er);
+    	}
+    	
+    	List<CoopDetails> coopDetails = new ArrayList<CoopDetails>();
+    	for (CoopDetailsDto coopDetailsDto : coopDetailsDtos) {
+    		CoopDetails cd = coopDetailsService.getCoopDetails(coopDetailsDto.getId());
+    		coopDetails.add(cd);
+    	}
+    	
+    	ec = employerContactService.updateEmployerContact(ec, firstName, lastName, email, phoneNumber, company, employerReports, coopDetails);
+    	
+    	return convertToDto(ec);
+    }
+    
+    @DeleteMapping("/employer/delete")
+    public void deleteEmployerContact(@RequestParam int id) {
+    	EmployerContact ec = employerContactService.getEmployerContact(id);
+    	employerContactService.deleteEmployerContact(ec);
+    }
+    
 
     private EmployerContactDto convertToDto(EmployerContact e) {
         if (e == null) {
