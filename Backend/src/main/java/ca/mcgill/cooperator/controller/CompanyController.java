1596def31acfeff8f5c551e2c5ce5f1c744f7eaf
Susan Matuszewski <susan.matuszewski@mail.mcgill.ca>
2020-01-17 10:47:21 -0500

    init start to implementing rest controller

--- Backend/src/main/java/ca/mcgill/cooperator/controller/CompanyController.java
+++ Backend/src/main/java/ca/mcgill/cooperator/controller/CompanyController.java
@@ -6,8 +6,10 @@ import ca.mcgill.cooperator.model.Company;
 import ca.mcgill.cooperator.model.EmployerContact;
 import ca.mcgill.cooperator.service.CompanyService;
 import ca.mcgill.cooperator.service.EmployerContactService;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- Backend/src/main/java/ca/mcgill/cooperator/controller/EmployerContactController.java
+++ Backend/src/main/java/ca/mcgill/cooperator/controller/EmployerContactController.java
@@ -12,9 +12,11 @@ import ca.mcgill.cooperator.service.CompanyService;
 import ca.mcgill.cooperator.service.CoopDetailsService;
 import ca.mcgill.cooperator.service.EmployerContactService;
 import ca.mcgill.cooperator.service.EmployerReportService;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,6 +24,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,11 +33,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("employer-contacts")
 public class EmployerContactController {
-
-    @Autowired EmployerContactService employerContactService;
-    @Autowired CompanyService companyService;
-    @Autowired EmployerReportService employerReportService;
-    @Autowired CoopDetailsService coopDetailsService;
+	
+	@Autowired EmployerContactService employerContactService;
+	@Autowired CompanyService companyService;
+	@Autowired EmployerReportService employerReportService;
+	@Autowired CoopDetailsService coopDetailsService;
 
     @GetMapping("/{id}")
     public EmployerContactDto getEmployerContactById(@PathVariable int id) {
@@ -47,6 +50,7 @@ public class EmployerContactController {
         List<EmployerContact> employerContacts = employerContactService.getAllEmployerContacts();
         return ControllerUtils.convertEmployerContactListToDto(employerContacts);
     }
+    
 
     @PostMapping("")
     public EmployerContactDto createEmployerContact(
