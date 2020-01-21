276f034261d09b1fa6a82e39f186f0203d358e12
Susan Matuszewski <susan.matuszewski@mail.mcgill.ca>
2020-01-21 13:05:19 -0500

    (#4) implemented student service and tests

--- Backend/src/main/java/ca/mcgill/cooperator/service/StudentService.java
+++ Backend/src/main/java/ca/mcgill/cooperator/service/StudentService.java
@@ -1,12 +1,14 @@
 package ca.mcgill.cooperator.service;
 
 import ca.mcgill.cooperator.dao.CoopRepository;
+import ca.mcgill.cooperator.dao.NotificationRepository;
 import ca.mcgill.cooperator.dao.StudentRepository;
 import ca.mcgill.cooperator.model.Coop;
 import ca.mcgill.cooperator.model.Notification;
 import ca.mcgill.cooperator.model.Student;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class StudentService {
     @Autowired StudentRepository studentRepository;
     @Autowired CoopRepository coopRepository;
+    @Autowired NotificationRepository notificationRepository;
 
     /**
      * create new student in database
@@ -39,7 +42,7 @@ public class StudentService {
         if (email == null || email.trim().length() == 0) {
             error.append("Student email cannot be empty! ");
         } else if (!ServiceUtils.isValidEmail(email)) {
-            error.append("Student email must be a valid email!");
+            error.append("Student email must be a valid email! ");
         }
         if (studentId == null || studentId.trim().length() == 0) {
             error.append("Student ID cannot be empty! ");
@@ -52,6 +55,7 @@ public class StudentService {
         s.setFirstName(firstName.trim());
         s.setLastName(lastName.trim());
         s.setEmail(email.trim());
+        s.setStudentId(studentId);
         s.setNotifications(new HashSet<Notification>());
         s.setCoops(new HashSet<Coop>());
 
@@ -61,6 +65,66 @@ public class StudentService {
     @Transactional
     public List<Student> getAllStudents() {
     	return ServiceUtils.toList(studentRepository.findAll());
+    }
+    
+    @Transactional
+    public Student updateStudent(Student s, 
+    						     String firstName, 
+    						     String lastName, 
+    						     String email, 
+    						     String studentId, 
+    						     Set<Coop> coops, 
+    						     Set<Notification> notifs) {
+    	
+    	StringBuilder error = new StringBuilder();
+    	if (s == null) {
+            error.append("Student to update cannot be null! ");
+        }
+        if (firstName == null || firstName.trim().length() == 0) {
+            error.append("Student first name cannot be empty! ");
+        }
+        if (lastName == null || lastName.trim().length() == 0) {
+            error.append("Student last name cannot be empty! ");
+        }
+        if (email == null || email.trim().length() == 0) {
+            error.append("Student email cannot be empty! ");
+        } else if (!ServiceUtils.isValidEmail(email)) {
+            error.append("Student email must be a valid email!");
+        }
+        if (studentId == null || studentId.trim().length() == 0) {
+            error.append("Student ID cannot be empty! ");
+        }
+        if (coops == null) {
+            error.append("Co-ops cannot be null! ");
+        }
+        if (notifs == null) {
+            error.append("Notifs cannot be null!");
+        }
+        if (error.length() > 0) {
+            throw new IllegalArgumentException(error.toString().trim());
+        }
+        
+        s.setFirstName(firstName.trim());
+        s.setLastName(lastName.trim());
+        s.setEmail(email.trim());
+        s.setStudentId(studentId);
+        s.setNotifications(notifs);
+        s.setCoops(coops);
+        
+        studentRepository.save(s);
+        
+        for (Notification notif : notifs) {
+        	notif.setStudent(s);
+        	notificationRepository.save(notif);
+        }
+        
+        for (Coop coop : coops) {
+        	coop.setStudent(s);
+        	coopRepository.save(coop);
+        }
+        
+        return studentRepository.save(s);
+    	
     }
     
     @Transactional
