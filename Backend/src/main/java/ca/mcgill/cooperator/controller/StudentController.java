7123b8af30f3eab1e05d8f5334c6f1d04e8d3b6a
PaulHooley <paul.hooley@mail.mcgill.ca>
2020-02-02 12:33:01 -0500

    Add studentController methods

--- Backend/src/main/java/ca/mcgill/cooperator/controller/StudentController.java
+++ Backend/src/main/java/ca/mcgill/cooperator/controller/StudentController.java
@@ -1,8 +1,17 @@
 package ca.mcgill.cooperator.controller;
 
+import ca.mcgill.cooperator.dto.StudentDto;
+import ca.mcgill.cooperator.model.Student;
+import ca.mcgill.cooperator.service.StudentService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +20,85 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("students")
 public class StudentController {
 
+    @Autowired private StudentService studentService;
+    /**
+     * Get all students
+     *
+     * @return List<StudentDto>
+     */
+    @GetMapping("")
+    public List<StudentDto> getAllStudents() {
+        List<Student> s = studentService.getAllStudents();
+
+        return ControllerUtils.convertToDto(s);
+    }
+    /**
+     * Get Student by id
+     *
+     * @param id
+     * @return StudentDto Object
+     */
     @GetMapping("/{id}")
-    public String getStudentById(@PathVariable int id) {
-        return "Hello World";
+    public StudentDto getStudentById(@PathVariable int id) {
+        Student s = studentService.getStudentById(id);
+
+        return ControllerUtils.convertToDto(s);
+    }
+    /**
+     * Create new Student
+     *
+     * <p>In request body:
+     *
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param studentId
+     * @return created Student
+     */
+    @PostMapping("")
+    public StudentDto createStudent(@RequestBody StudentDto s) {
+        Student student =
+                studentService.createStudent(
+                        s.getFirstName(), s.getLastName(), s.getEmail(), s.getStudentId());
+
+        return ControllerUtils.convertToDto(student);
+    }
+
+    /**
+     * Update student
+     *
+     * <p>In request body:
+     *
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param studentId
+     * @param List<Coops>
+     * @param List<Notifications>
+     * @return updated Student
+     */
+    @PutMapping("")
+    public StudentDto updateStudent(@RequestBody StudentDto s) {
+        Student student = studentService.getStudentById(s.getId());
+        studentService.updateStudent(
+                student,
+                s.getFirstName(),
+                s.getLastName(),
+                s.getEmail(),
+                s.getStudentId(),
+                ControllerUtils.convertCoopsListToDomainObject(s.getCoops()),
+                ControllerUtils.convertNotificationListToDomainObjectSet(s.getNotifications()));
+        return null;
+    }
+    /**
+     * Deletes an existing student
+     *
+     * @param id
+     * @return deleted Student
+     */
+    @DeleteMapping("/{id}")
+    public StudentDto deleteStudent(@PathVariable int id) {
+        Student student = studentService.deleteStudent(studentService.getStudentById(id));
+        return ControllerUtils.convertToDto(student);
     }
 }
