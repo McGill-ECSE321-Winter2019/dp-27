6e68529857e52123ce4d509b9da4af08c07d7aa8
kraglalbert <albert.kragl@mail.mcgill.ca>
2020-02-07 14:00:53 -0500

    Fix DB config

--- Backend/src/main/java/ca/mcgill/cooperator/config/DataSourceConfig.java
+++ Backend/src/main/java/ca/mcgill/cooperator/config/DataSourceConfig.java
@@ -6,14 +6,17 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("default")
 public class DataSourceConfig {
 
     @Primary
     @Bean
     public DataSource getDataSource() {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.url(dotenv.get("DATASOURCE_URL"));
         dataSourceBuilder.username(dotenv.get("DATASOURCE_USERNAME"));
