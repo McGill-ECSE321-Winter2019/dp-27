b4ce07d94ad194451cd1a6af4d3705d91276fbe9
Albert Kragl <albert.kragl@mail.mcgill.ca>
2020-02-17 20:04:54 -0500

    Add collapsible left menu to Admin and Student layouts (#91)
    * Add collapsible left menu to Admin and Student layouts
    
    * Remove random MainLayout file

---
+++ Frontend/src/components/SidebarLink.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-item clickable tag="a" @click="goToLink">
+    <q-item-section v-if="icon" avatar>
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+      <q-item-label caption>{{ caption }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  name: "SidebarLink",
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    caption: {
+      type: String,
+      default: ""
+    },
+    link: {
+      type: String,
+      default: "#"
+    },
+    icon: {
+      type: String,
+      default: ""
+    }
+  },
+  methods: {
+    goToLink: function() {
+      this.$router.push({ path: this.link });
+    }
+  }
+};
+</script>
