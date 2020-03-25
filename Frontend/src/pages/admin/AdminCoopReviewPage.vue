2c53147608fc8df60f44fd8eb65b76b6548924fb
kraglalbert <albert.kragl@mail.mcgill.ca>
2020-03-25 11:58:50 -0400

    Add some more UI elements

--- Frontend/src/pages/admin/AdminCoopReviewPage.vue
+++ Frontend/src/pages/admin/AdminCoopReviewPage.vue
@@ -14,8 +14,8 @@
             align="justify"
             narrow-indicator
           >
-            <q-tab name="new_coops" label="New Co-ops" />
-            <q-tab name="completed_coops" label="Completed Co-ops" />
+            <q-tab name="new_coops" label="New Co-ops"></q-tab>
+            <q-tab name="completed_coops" label="Completed Co-ops"></q-tab>
           </q-tabs>
         </q-card>
 
@@ -27,13 +27,33 @@
         <div v-else>
           <q-tab-panels v-model="currentTab" animated>
             <q-tab-panel name="new_coops">
-              <CoopReviewPageNewCoopItem
-                v-for="coop in newCoops"
-                :key="coop.id"
-                :coop="coop"
-              />
+              <div v-if="newCoops.length > 0">
+                <div class="text-subtitle1 q-mb-lg">
+                  <span class="text-weight-medium"
+                    >Number of new co-ops to review:
+                  </span>
+                  <q-badge align="middle" color="black">
+                    {{ newCoops.length }}
+                  </q-badge>
+                </div>
+                <CoopReviewPageNewCoopItem
+                  v-for="coop in newCoops"
+                  :key="coop.id"
+                  :coop="coop"
+                />
+              </div>
+              <div v-else class="center-item">
+                No new co-ops to review at this time.
+              </div>
             </q-tab-panel>
-            <q-tab-panel name="completed_coops"></q-tab-panel>
+            <q-tab-panel name="completed_coops">
+              <div v-if="completedCoops.length > 0">
+                <!-- TODO: implement this -->
+              </div>
+              <div v-else class="center-item">
+                No completed co-ops to review at this time.
+              </div>
+            </q-tab-panel>
           </q-tab-panels>
         </div>
       </div>
