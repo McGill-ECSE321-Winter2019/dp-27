d08bd0780c0bff5d0061003cc44950a1da8bd3fd
kraglalbert <albert.kragl@mail.mcgill.ca>
2020-03-29 13:33:19 -0400

    Add log4j for proper logging

--- Backend/src/main/java/ca/mcgill/cooperator/schedule/NightlyTasks.java
+++ Backend/src/main/java/ca/mcgill/cooperator/schedule/NightlyTasks.java
@@ -4,9 +4,11 @@ import ca.mcgill.cooperator.model.Coop;
 import ca.mcgill.cooperator.model.CoopStatus;
 import ca.mcgill.cooperator.service.CoopService;
 import java.sql.Date;
-import java.text.SimpleDateFormat;
 import java.util.List;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +17,12 @@ import org.springframework.stereotype.Component;
  * should be specified with a cron expression.
  */
 @Component
+@EnableAsync
 public class NightlyTasks {
 
     @Autowired CoopService coopService;
+
+    static final Logger logger = Logger.getLogger(NightlyTasks.class);
 
     // runs once every weekday at 4am
     static final String CRON_WEEKDAY = "0 0 4 * * MON-FRI";
@@ -26,6 +31,7 @@ public class NightlyTasks {
     static final String CRON_TEST = "0 */5 * ? * *";
 
     /** Checks all existing Coops and updates their status if necessary. */
+    @Async
     @Scheduled(cron = CRON_TEST)
     public void updateCoopStatuses() {
         List<Coop> coops = coopService.getAllCoops();
@@ -33,9 +39,9 @@ public class NightlyTasks {
         Date currentDate = new Date(System.currentTimeMillis());
         Date coopDate;
 
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm");
-        System.out.println(
-                df.format(currentDate) + " [Schedule] Running job to update Coop statuses");
+        if (logger.isInfoEnabled()) {
+            logger.info("Running job to update Coop statuses");
+        }
 
         for (Coop coop : coops) {
             if (coop.getStatus() == CoopStatus.FUTURE) {
@@ -50,12 +56,16 @@ public class NightlyTasks {
                 }
             } else if (coop.getStatus() == CoopStatus.IN_PROGRESS) {
                 coopDate = coop.getCoopDetails().getEndDate();
-                // check if the co-op ended today or has already ended
-                if (currentDate.compareTo(coopDate) >= 0) {
+                // check if the co-op has ended
+                if (currentDate.compareTo(coopDate) > 0) {
                     // update status to FINISHED
                     coopService.updateCoop(coop, CoopStatus.FINISHED, null, null, null, null, null);
                 }
             }
+        }
+
+        if (logger.isInfoEnabled()) {
+            logger.info("Completed job to update Coop statuses");
         }
     }
 }
