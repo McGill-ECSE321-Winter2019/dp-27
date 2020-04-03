decc494ac246776bf62ced576549963093635b31
kraglalbert <albert.kragl@mail.mcgill.ca>
2020-04-03 15:11:18 -0400

    Rename BaseController exception handlers

--- Backend/src/main/java/ca/mcgill/cooperator/controller/BaseController.java
+++ Backend/src/main/java/ca/mcgill/cooperator/controller/BaseController.java
@@ -5,14 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 public class BaseController {
-	
+
     @ExceptionHandler(IllegalArgumentException.class)
-    public final ResponseEntity<String> handleAll4XXExceptions(IllegalArgumentException ex) {
+    public final ResponseEntity<String> handleAllServiceExceptions(IllegalArgumentException ex) {
         return new ResponseEntity<String>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public final ResponseEntity<Exception> handleAllExceptions(RuntimeException ex) {
+    public final ResponseEntity<Exception> handleAllRuntimeExceptions(RuntimeException ex) {
         ex.printStackTrace();
         return new ResponseEntity<Exception>(ex, HttpStatus.INTERNAL_SERVER_ERROR);
     }
