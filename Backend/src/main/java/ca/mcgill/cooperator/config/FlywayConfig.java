8f0a00e42eaed220280748fdf2bcff82e77f25f8
Susan Matuszewski <susan.matuszewski@mail.mcgill.ca>
2020-04-08 10:05:57 -0400

    removed flyway clean from default config

--- Backend/src/main/java/ca/mcgill/cooperator/config/FlywayConfig.java
+++ Backend/src/main/java/ca/mcgill/cooperator/config/FlywayConfig.java
@@ -15,7 +15,6 @@ public class FlywayConfig {
 	    FlywayMigrationStrategy strategy = new FlywayMigrationStrategy() {
 	        @Override
 	        public void migrate(Flyway flyway) {
-	            flyway.clean();
 	            flyway.migrate();
 	        }
 	    };
